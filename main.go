2af34cf74e2e497e2fd6389ee71945482bf6d106
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2024-02-27 01:37:19 -0300

    feat: create a connection with localhost and present the content in the screen

--- main.go
+++ main.go
@@ -9,5 +9,10 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./public"))
 	mux := http.NewServeMux()
 	mux.Handle("/", fileServer)
+	mux.HandleFunc("/blog", helloFunc)
 	log.Fatal(http.ListenAndServe(":3333", mux))
+}
+
+func helloFunc(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("Hello From Blog"))
 }
